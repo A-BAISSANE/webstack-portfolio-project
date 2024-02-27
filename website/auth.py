1282b65bb675e93119106abb908a8e2cc888bccc
Ahmed Baissane <baissanee@gmail.com>
2024-02-27 20:29:35 +0100

    Updates

--- website/auth.py
+++ website/auth.py
@@ -55,8 +55,8 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name)
+            new_user.set_password(password1)  # Use set_password method to hash the password
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
